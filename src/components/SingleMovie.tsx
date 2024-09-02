88a38284fcc5b801ab7b0b13c4df0969a5fdd1c5
WalterAurelio <aurelio.gareca.94@hotmail.com>
2024-09-01 22:11:46 -0300

    Se modificó la implementación del componente SingleMovie, de manera que ahora muestre más información y de manera dinámica.

--- src/components/SingleMovie.tsx
+++ src/components/SingleMovie.tsx
@@ -1,22 +1,58 @@
 import { Link } from '@tanstack/react-router';
 import { MovieDiscover } from '../api/movies';
+import { useGetGenres } from '../services/queries';
+import { MovieGenre } from '../api/genres';
 
 function SingleMovie({ movie }: { movie: MovieDiscover }) {
+  const { data } = useGetGenres();
+
+  const movieGenres = (genresIds: number[] | undefined): MovieGenre[] | undefined => {
+    if (genresIds && data) {
+      const genreMatches: MovieGenre[] = [];
+      for (let i = 0; i < genresIds.length; i++) {
+        const foundGenre = data.find(genre => genre.id === genresIds[i]);
+        if (foundGenre) {
+          genreMatches.push(foundGenre);
+        }
+      }
+      return genreMatches;
+    }
+  };
+
   return (
-    <Link
-      to={`/movies/watch/$movie`}
-      params={{
-        movie: JSON.stringify({ id: movie.id, title: movie.title }),
-      }}
-    >
-      <div>
+    <div>
+      <Link
+        to={`/movies/watch/$movie`}
+        params={{
+          movie: JSON.stringify({ id: movie.id, title: movie.title }),
+        }}
+      >
         <img
           src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
           alt={`Poster de ${movie.title}`}
         />
-      </div>
-      <h3>{movie.title}</h3>
-    </Link>
+      </Link>
+      <p>{movie.release_date.slice(0, 4)}</p>
+      {movieGenres(movie.genre_ids)?.map(genre => (
+        <Link
+          to='/movies/discover'
+          search={{
+            page: 1,
+            with_genres: [genre.id],
+          }}
+        >
+          {genre.name}
+        </Link>
+      ))}
+      <Link
+        to={`/movies/watch/$movie`}
+        params={{
+          movie: JSON.stringify({ id: movie.id, title: movie.title }),
+        }}
+      >
+        <h3>{movie.title}</h3>
+      </Link>
+    </div>
   );
 }
 
